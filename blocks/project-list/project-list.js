073892b882968228174d84f9d5af277ed5efda91
priyasri-1403 <priyasr@adobe.com>
2025-03-24 11:05:56 +0530

    Implemented Show/Hide column

--- blocks/project-list/project-list.js
+++ blocks/project-list/project-list.js
@@ -1,38 +1,31 @@
 export default function decorate(block) {
-  console.log(document); 
-  
-  const navBox = document.querySelector('.nav-wrapper');
-  console.log(navBox);
-  // if (navBox.querySelector('[aria-expanded = "true"]')) {
-  //   document.querySelector('.project-list-container').classList.add('table-right-shift');
-  // }
 
   const tableContainer = block.querySelector('div');
   const url = block.querySelector('.button-container a');
   
+  const lastElement = block.lastElementChild;
+  const regions = lastElement.querySelector('p').textContent;
+  const regionList = regions.split(',').map((region) => region.trim());
+  console.log(regionList);
+
+  lastElement.remove();
+  
   setTimeout(() => {
     const navButton = document.querySelector('.nav-hamburger button');
     const projectContainer = block.closest('.project-list-container');
+    const nav = document.getElementById('nav');
     
     if (navButton && projectContainer) {
       // Initial check
-      const nav = document.getElementById('nav');
-      if (nav && nav.getAttribute('aria-expanded') === 'true') {
+      if(nav.getAttribute('aria-expanded') === 'true'){
         projectContainer.classList.add('table-right-shift');
       }
-      
       navButton.addEventListener('click', () => {
-        setTimeout(() => {
-          const isExpanded = nav.getAttribute('aria-expanded') === 'true';
-          if (isExpanded) {
-            projectContainer.classList.add('table-right-shift');
-          } else {
-            projectContainer.classList.remove('table-right-shift');
-          }
-        }, 50);
+        projectContainer.classList.toggle("table-right-shift");      
+      
       });
     }
-  }, 500);
+  }, 150);
 
   //   console.log(url);
 
@@ -54,15 +47,26 @@ export default function decorate(block) {
   tableContainer.innerHTML = `
        
         <div class="input-container">
-        <input type="text" id="searchBox" placeholder="Search" />
-         <img class = "search-icon" src="../../icons/search.svg"/>
-        </div>
+           <input type="text" id="searchBox" placeholder="Search" />
+           <img class = "search-icon" src="../../icons/search.svg"/>
+           <div class = "filter-box">
+             <select id="regionDropdown">
+              <option value="">Select Region</option>
+             </select>
+
+           <div class = 'column-filterBox'> <img src="../../icons/filter-list.svg"/>
+            <div id='column-filters' class = "toggle-column hide">Show/Hide column</div>
+           </div>
+          </div>
+          </div>
         <div id="myGrid" class="ag-theme-alpine" style="height: 580px;"></div>
     `;
 
   const searchBox = block.querySelector('#searchBox');
   const gridDiv = block.querySelector('#myGrid');
-  const nonHeaders = ['Region (Project)',
+  const regionDropdown = block.querySelector('#regionDropdown');
+
+  const nonHeaders = [
     'Project Status Notes (Project)',
     'Status As on 3rd Mar 2025',
     'Status As on 10th Mar 2025',
@@ -70,6 +74,7 @@ export default function decorate(block) {
   ];
 
   fetchProjectData().then((data) => {
+    console.log(data)
     if (data.length === 0) {
       console.warn('No data available for AG Grid.');
       return;
@@ -97,5 +102,46 @@ export default function decorate(block) {
     const gridApi = agGrid.createGrid(gridDiv, gridOptions);
 
     searchBox.addEventListener('input', () => gridApi.setGridOption('quickFilterText', document.getElementById('searchBox').value));
+
+    regionList.forEach(region => {
+      const option = document.createElement("option");
+      option.value=region;
+      option.textContent = region;
+      regionDropdown.appendChild(option);
+    })
+   
+    regionDropdown.addEventListener('change',() =>{
+      const selectedRegion = regionDropdown.value;
+      gridApi.setGridOption("rowData",selectedRegion ? data.filter(row => row["Region (Project)"] === selectedRegion) : data)
+    })
+
+    const columnFilterBox = document.querySelector('.column-filterBox');
+    const columnCheckBox = document.getElementById('column-filters');
+    // columnFilterBox.addEventListener('click',columnCheckBox.classList.toggle('hide'))
+
+    columnDefs.forEach((col) => {
+      const checkbox = document.createElement('input');
+      checkbox.type='checkbox';
+      checkbox.checked=!col.hide; 
+      checkbox.dataset.field = col.field;
+
+      const label = document.createElement('label');
+      label.textContent = col.headerName;
+      label.appendChild(checkbox);
+      columnCheckBox.appendChild(label);
+
+      checkbox.addEventListener('change',() =>{
+        gridApi.applyColumnState({
+          state: [{ colId: col.field, hide: !checkbox.checked }],
+          applyOrder: true,
+        });
+      });
+
+    });
+
+
   });
+ 
+ 
+  
 }
